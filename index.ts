aa23932d90f1d47344e3d4fe7b8c4d16793dab89
kitoumasaomi <kanfutrooper@gmail.com>
2025-09-19 13:50:54 +0900

    index.tsを作成

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import * as http from 'http';
 
-const server = http.createServer(async (req, res)) => {};
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Hello World');
+});
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
