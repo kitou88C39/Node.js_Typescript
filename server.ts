57fab8228ecaa63feefc9ab010d7d303cd13dfde
kitoumasaomi <kanfutrooper@gmail.com>
2025-09-20 11:45:42 +0900

    server.tsを作成

--- server.ts
+++ server.ts
@@ -17,6 +17,11 @@ app.get('/users/:id', (req, res) => {
   res.send(`User Id is ${req.params.id}.Name is ${req.query.name}`);
 });
 
+//Postメソッドの処理の書き方
+app.post('/', (req, res) => {
+  res.send('This is a POST request');
+});
+
 app.listen(PORT, () => {
   console.log('サーバーが起動しました');
 });
